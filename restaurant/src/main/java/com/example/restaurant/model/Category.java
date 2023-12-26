5599a025bfae5122ef6279a7590a3c021d39f8cd
MohamedAliAyoub <mohamedali163163@gmail.com>
2023-12-26 23:07:48 +0200

    add timestamp in tables

--- restaurant/src/main/java/com/example/restaurant/model/Category.java
+++ restaurant/src/main/java/com/example/restaurant/model/Category.java
@@ -1,10 +1,13 @@
 package com.example.restaurant.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.util.Date;
 import java.util.Set;
 
 @Data
@@ -14,5 +17,7 @@ public class Category extends CategoryOrder{
 
     @OneToMany(mappedBy = "category")
     private Set<Order> orders;
+
+
 }
 
--- restaurant/src/main/java/com/example/restaurant/model/CategoryOrder.java
+++ restaurant/src/main/java/com/example/restaurant/model/CategoryOrder.java
@@ -3,6 +3,10 @@ package com.example.restaurant.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
 
 // Id
 // name
@@ -13,4 +17,12 @@ import lombok.*;
 public class CategoryOrder extends BaseEntity{
     @Column(name = "name")
     private String name;
+
+   @Column(name = "data_create")
+    @CreationTimestamp
+    private Date dateCreate;
+
+    @Column(name = "data_update")
+    @UpdateTimestamp
+    private Date dateUpdate;
 }
