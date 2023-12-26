ce149552dab15a68c29c69b887e6733600bb37ee
MohamedAliAyoub <mohamedali163163@gmail.com>
2023-12-27 00:50:37 +0200

    add repositories

---
+++ restaurant/src/main/java/com/example/restaurant/deo/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.restaurant.deo;
+
+import com.example.restaurant.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
