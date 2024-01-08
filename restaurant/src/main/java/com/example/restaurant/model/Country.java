e5582b9183c8e3e635585c23b8db2e42d88c6754
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-08 23:38:57 +0200

    Relationship between Model (Country & State)

--- restaurant/src/main/java/com/example/restaurant/model/Country.java
+++ restaurant/src/main/java/com/example/restaurant/model/Country.java
@@ -2,10 +2,14 @@ package com.example.restaurant.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
 
 @Entity
 @Table(name = "country")
@@ -15,4 +19,7 @@ import lombok.NoArgsConstructor;
 public class Country extends PublicData {
     @Column(name = "code")
     private String code;
+
+    @OneToMany(mappedBy = "country")
+    private Set<State> states;
 }
--- restaurant/src/main/java/com/example/restaurant/model/State.java
+++ restaurant/src/main/java/com/example/restaurant/model/State.java
@@ -1,9 +1,15 @@
 package com.example.restaurant.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name="state")
 public class State extends PublicData{
+
+    @ManyToOne
+    @JoinColumn(name = "country_id")
+    private Country country;
 }
