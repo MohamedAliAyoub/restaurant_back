235f842d8d1d988ab2b8182ed7b652ea2ba5e625
MohamedAliAyoub <mohamedali163163@gmail.com>
2024-01-08 23:26:58 +0200

    @MappedSuperclass to solve name

--- restaurant/src/main/java/com/example/restaurant/model/Country.java
+++ restaurant/src/main/java/com/example/restaurant/model/Country.java
@@ -3,9 +3,15 @@ package com.example.restaurant.model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "country")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Country extends PublicData {
     @Column(name = "code")
     private String code;
--- restaurant/src/main/java/com/example/restaurant/model/PublicData.java
+++ restaurant/src/main/java/com/example/restaurant/model/PublicData.java
@@ -1,9 +1,9 @@
 package com.example.restaurant.model;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
+import jakarta.persistence.MappedSuperclass;
 
-@Entity
+@MappedSuperclass
 public class PublicData extends BaseEntity {
     @Column(name = "name")
     private String name;
